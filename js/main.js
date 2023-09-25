600328c86231ac459fc8314b2deb63e6cde6b3fd
ARealConner <81334211+ARealConner@users.noreply.github.com>
2023-09-25 13:19:18 -0400

    Refactor CSS styles and JavaScript code for better readability and maintainability.
    - Refactored CSS styles to use universal selector for margin, padding, and box-sizing properties.
    - Added visually-hidden class to hide elements from screen readers.
    - Updated button styles with padding, border-radius, cursor, transition, and box-shadow properties.
    - Updated body styles with font-family, transition, margin, and padding properties.
    - Added data-theme attribute to customize button and textarea styles based on light or dark theme.
    - Adjusted calculator container width and margin for better layout.
    - Added event listener to inputBox element in main.js file to send input data to the backend server for processing.
    - Created processInput function in main.js file to handle the input processing logic asynchronously with retries.
    - Created sendProcessRequest function in main.js file to make a POST request to the backend API with the input data as JSON payload.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,72 @@
-document.getElementById('inputBox').addEventListener('input', function() {
-  let inputData = this.value;
+let inputBox;
+let outputBox;
 
-  // Send the current input data to the backend server for processing
-  fetch('http://localhost:8080/process', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ data: inputData })
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      // Assuming the server responds with a processedInput and an output field
-      // Update the input and output boxes with the data received from the server
-      document.getElementById('inputBox').value = data.processedInput;
-      document.getElementById('outputBox').value = data.output;
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error.message);
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  inputBox = document.getElementById("inputBox");
+  outputBox = document.getElementById("outputBox");
+
+  inputBox.addEventListener("input", function() {
+    const inputText = inputBox.value;
+
+    // Call the backend API
+    processInput(inputText);
+  });
 });
 
+async function processInput(inputText) {
+  for (let i = 0; i < 3; i++) {
+    let ok = false;
+    // Call the backend API
+    sendProcessRequest(inputText)
+      .then(data => {
+        // Accessing individual elements:
+        const processedInput = data['processedInput'];
+        const processedOutput = data['processedOutput'];
 
+        console.log("Processed Data:", data);
+        console.log("inputText:", inputText);
+        console.log("Processed Input:", processedInput);
+        console.log("Processed Output:", processedOutput);
+
+        inputBox.value = processedInput;
+        outputBox.value = processedOutput;
+        ok = true;
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+    if (ok) {
+      break;
+    }
+    console.log("Retrying..." + i);
+    // wait for 1 second before trying again
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
+async function sendProcessRequest(inputText) {
+  const response = await fetch('https://solverservice-dfab67be627f.herokuapp.com/processInput', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ input: inputText }),
+  });
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error('Failed to process input');
+  }
+}
+
+// To use this function:
+// const inputText = "\n";
+// sendProcessRequest(inputText)
+//   .then(data => {
+//     // Update your frontend UI here
+//     console.log("Processed Data:", data);
+//   })
+//   .catch(error => {
+//     console.error("Error:", error);
+//   });
