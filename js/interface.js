523ccc50dab1a6ea308b7750c84345ba22c6d7ad
ARealConner <81334211+ARealConner@users.noreply.github.com>
2023-10-11 15:31:09 -0400

    feat: Update CSS and HTML for about page
    - Added styles for the header, h1, nav, and theme toggle button
    - Updated the layout of the header to use grid
    - Adjusted padding and alignment of elements in the header
    - Added styles for the footer and theme toggle container
    - Updated body styles to use flexbox and set a minimum height
    - Added width property to iframes for better responsiveness
    
    This commit updates the CSS file "about.css" and HTML files "about.html" and "how-to-use.html" to improve the styling and layout of the about page. The changes include adding new styles for various elements such as headers, navigation, buttons, footers, and iframes. The layout of the header has been updated using grid properties. The body now uses flexbox for better responsiveness.

--- js/interface.js
+++ js/interface.js
@@ -1,30 +1,55 @@
 const themeToggle = document.getElementById("themeToggle");
-const currentTheme = localStorage.getItem("theme") || "dark"; // Changed 'light' to 'dark' here
+let currentTheme = localStorage.getItem("theme") || "dark";
 
+// Apply theme to document
 document.documentElement.setAttribute("data-theme", currentTheme);
-if (currentTheme === "light") {
-  themeToggle.textContent = "Dark Mode";
-} else {
+if (currentTheme === "dark") {
   themeToggle.textContent = "Light Mode";
+} else {
+  themeToggle.textContent = "Dark Mode";
 }
 
+// Function to update iframe themes
+const updateIframeThemes = (newTheme) => {
+  document.querySelectorAll("iframe").forEach((iframe) => {
+    try {
+      iframe.contentWindow.document.documentElement.setAttribute(
+        "data-theme",
+        newTheme,
+      );
+    } catch (e) {
+      console.error("Could not set theme for iframe:", e);
+    }
+  });
+};
+
+// Event listener for theme toggle button
 themeToggle.addEventListener("click", () => {
-  let currentTheme = document.documentElement.getAttribute("data-theme");
-  if (currentTheme === "light") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    themeToggle.textContent = "Light Mode";
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    themeToggle.textContent = "Dark Mode";
-    localStorage.setItem("theme", "light");
-  }
+  currentTheme = document.documentElement.getAttribute("data-theme");
+  let newTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", newTheme);
+  themeToggle.textContent = newTheme === "dark" ? "Light Mode" : "Dark Mode";
+  localStorage.setItem("theme", newTheme);
+  updateIframeThemes(newTheme);
 });
 
+// Event listener for DOMContentLoaded
 window.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const hideHeader = urlParams.get("hideHeader");
   if (hideHeader === "true") {
     document.querySelector("header").style.display = "none";
   }
+  updateIframeThemes(currentTheme);
 });
+
+// Update theme for embedded iframes if this page is within an iframe
+if (window.self !== window.top) {
+  try {
+    const parentTheme =
+      window.parent.document.documentElement.getAttribute("data-theme");
+    document.documentElement.setAttribute("data-theme", parentTheme);
+  } catch (e) {
+    console.error("Could not get theme from parent:", e);
+  }
+}
